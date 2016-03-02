ce57e09e2936d01f4d67f8250614f9c2a82e1070
TomNash <nashtf@g.cofc.edu>
2016-03-02 14:23:05 -0500

    Remove print from debugging

--- R/runSAM.R
+++ R/runSAM.R
@@ -52,7 +52,6 @@ runSAM <- function(backgroundSub.obj,response,delta) {
   # samr.compute.siggenes.table flips genename and geneid
   colnames(desc.dataSAM)[c(backgroundSub.obj$symbolIndex,
                            backgroundSub.obj$idInd)] <- c("geneid","genenames")
-  print(head(desc.dataSAM))
   siggenes.table <- samr::samr.compute.siggenes.table(samr.obj,delta,desc.dataSAM,
                                                 delta.table,compute.localfdr=TRUE)
   
