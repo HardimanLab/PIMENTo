2061c5d0741c3d164ccbd3f76689b75d397af6d1
TomNash <nashtf@g.cofc.edu>
2016-04-20 14:41:32 -0400

    Change 0 to 1 for log2 transform in backgroundCutoff

--- R/backgroundCutoff.R
+++ R/backgroundCutoff.R
@@ -24,6 +24,9 @@ backgroundCutoff <- function(preprocessData.obj, method,
     stop("Input argument 'method' must be either 'quantile' or 'mloess'")
   
   geneMax <- apply(backgroundData,1,max)
+  mat.geneMax <- as.matrix(geneMax)
+  mat.geneMax[mat.geneMax==0] <- 1
+  geneMax <- as.data.frame(mat.geneMax)
   logData <- log2(geneMax)
   
   if(xlim.hi != 0 | xlim.lo != 0) {
